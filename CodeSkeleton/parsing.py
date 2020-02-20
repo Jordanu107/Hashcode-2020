e1484e08a89717fbe5d2e58aa0caaeb119b716c2
btlin1213 <55481667+btlin1213@users.noreply.github.com>
2020-02-21 05:34:51 +1100

    Update parsing.py

--- CodeSkeleton/parsing.py
+++ CodeSkeleton/parsing.py
@@ -1,4 +1,20 @@
 # dataset = {
+    # 'nbooks' = number of books in total
+    # 'nlib' = number of libraries in total
+    # 'days' = number of days available
+    # 'scores' = a list of scores with its index corresponding to the book number
+    # (so scores = [1, 2, 3] means book 0 has score 1 etc)
+
+    # every (key, value) pair after this represent a library
+    # 'lib_id': [lib_nbooks, lib_signup, lib_ship, [lib_books]]
+    # example:
+    #     '0': [5, 2, 2, [0, 1, 2, 3, 4]]
+    # library 0 has 5 books, takes 2 days to signup, ship 2 books per day,
+    # library 0 has books indexed 0, 1, 2, 3, 4 
+    #     (use this same index to find its score in scores list from line 6)
+# }
+
+# dataset = { this is the sample input
 #     'nbooks': 6
 #     'nlib': 2
 #     'ndays': 7
