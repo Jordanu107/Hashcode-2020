9d419fb1d222b5b909fd7b929417cfacd688c1ab
Colton Carner <ccarner13@gmail.com>
2020-02-20 16:39:33 +1100

    reworked implementation

--- Practice Problem/solution.py
+++ Practice Problem/solution.py
@@ -5,7 +5,6 @@
 
 #### https://www.ics.uci.edu/~pattis/ICS-33/lectures/complexitypython.txt
 
-FILE_NAME = 'a_example.in'
 
 # def find_optimal_slices(filename):
 #     max_slices = 0
@@ -68,63 +67,112 @@ def find_optimal_slices(filename):
         # Read how many slices each pizza contains
         pizza_slices = [int(x) for x in next(file).split()]
 
-    pizza_list = [[0] * (max_slices + 1) for _ in range(len(pizza_slices))]
+    
+    pizza_list = [ [0] * (max_slices + 1) for _ in range(num_of_pizzas)]
+
+    print (pizza_list)
 
     # 'hard code' for the first column/item
-    for capacity in range(max_slices):
+    for capacity in range(max_slices+1):
         current_slice = pizza_slices[0]
 
         if capacity < current_slice:
             pizza_list[0][capacity] = 0
-        elif capacity == current_slice:
-            pizza_list[0][capacity] = current_slice
         else:
-            pizza_list[0][capacity] = pizza_list[0][capacity - 1]
+            pizza_list[0][capacity] = current_slice
+       
+        # elif capacity == current_slice:
+        #     pizza_list[0][capacity] = current_slice
+        # else:
+        #     pizza_list[0][capacity] = pizza_list[0][capacity - 1]
 
     # Fill out the highest possible amount of pizza given the pizza slice
     # capacity and whether the current pizza is eaten or not
+
     for pizza in range(1, len(pizza_list)):
         current_slice = pizza_slices[pizza]
+
+        # if (pizza == 1):
+        #     print("curslice = " + current_slice)
 
         for capacity in range(max_slices + 1):
 
             # Capacity too small, just put what the largest could fit in capacity - 1
             if capacity < current_slice:
+                # if (pizza == 1 ):
+                    # print("capacity was less than current_slice" :)
                 pizza_list[pizza][capacity] = pizza_list[pizza - 1][capacity]
 
-            # Just the right size for the pizza!
-            elif capacity == current_slice:
-                pizza_list[pizza][capacity] = current_slice
-
-            # Have more capacity for pizza slices than the current slice of pizza
             else:
+                pizza_list[pizza][capacity] = max(pizza_list[pizza - 1][capacity], pizza_list[pizza - 1][capacity-current_slice] + current_slice)
 
-                if pizza_list[pizza][capacity - current_slice] + current_slice <= max_slices:
-                    pizza_list[pizza][capacity] = max(pizza_list[pizza - 1][capacity],
-                                                      pizza_list[pizza - 1][capacity - current_slice] +
-                                                      current_slice)
-                else:
-                    pizza_list[pizza][capacity] = pizza_list[pizza - 1][capacity]
+            
 
-    # Back traverse through to find the pizzas that were consumed
+            # # Just the right size for the pizza!
+            # elif capacity == current_slice:
+            #     pizza_list[pizza][capacity] = current_slice
+
+            # # Have more capacity for pizza slices than the current slice of pizza
+            # else:
+
+            #     if pizza_list[pizza][capacity - current_slice] + current_slice <= max_slices:
+            #         pizza_list[pizza][capacity] = max(pizza_list[pizza - 1][capacity],
+            #                                           pizza_list[pizza - 1][capacity - current_slice] +
+            #                                           current_slice)
+            #     else:
+            #         pizza_list[pizza][capacity] = pizza_list[pizza - 1][capacity]
+
+    for list in pizza_list:
+        print(list);
+
+
+    # # Back traverse through to find the pizzas that were consumed
     highest_pizza_value = pizza_list[-1][-1]
+    current_column = max_slices
+    pizzas_chosen = []
 
     indexes = []
-    for i in range(len(pizza_list) - 1, -1, -1):
 
-        # The pizza before it, MUST have been used to compute the highest pizza value
-        if highest_pizza_value not in pizza_list[i - 1]:
-            indexes.append(i)
-            highest_pizza_value -= pizza_slices[i]
+    # Don't look a tthe first pizza row, account for below
+    for i in range(len(pizza_list) - 1, 0, -1):
 
-    if max(pizza_list[0]) == highest_pizza_value:
-        indexes.append(0)
+        # # The pizza before it, MUST have been used to compute the highest pizza value
+        # if highest_pizza_value not in pizza_list[i - 1]:
+        #     indexes.append(i)
+        #     highest_pizza_value -= pizza_slices[i]
 
-    print(len(indexes))
-    print(" ".join(str(index) for index in indexes[::-1]))
+        try:
+            if pizza_list[i-1][current_column] != highest_pizza_value:
+                # DID choose this pizza
+                current_column = current_column - pizza_slices[i]
+                highest_pizza_value = highest_pizza_value - pizza_slices[i]
+                pizzas_chosen.append(i)
+        except IndexError:
+            print("error was" + str(i-1) + " " + str(current_column))
+            print(pizzas_chosen)
+        
+
+    # now consider the first pizza
+    if pizza_list[0][current_column] != 0:
+        pizzas_chosen.append(0)
+
+    total_score = 0
+    for pizza in pizzas_chosen:
+        total_score += pizza_slices[pizza]
+
+
+
+    # if max(pizza_list[0]) == highest_pizza_value:
+    #     indexes.append(0)
+
+    print(len(pizzas_chosen))
+    print(" ".join(str(pizza_number) for pizza_number in pizzas_chosen))
+    print("total score is")
+    print(total_score)
 
 
 # find_optimal_slices(FILE_NAME)
+# find_optimal_slices('a_example.in')
 # find_optimal_slices('b_small.in')
 # find_optimal_slices('c_medium.in')
 find_optimal_slices('d_quite_big.in')
